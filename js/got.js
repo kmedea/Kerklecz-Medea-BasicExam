c41899d75073e38e2f5038dcf44d31787c7e5107
Kerklecz Médea <kerklecz.medea@gmail.com>
2018-12-09 22:41:14 +0100

    search is really ok

--- js/got.js
+++ js/got.js
@@ -12,11 +12,12 @@ function getGameOfThronesCharacterDatas(url, callbackFunc) {
 function successGetGameOfThronesCharacterDatas(xhttp) {
   // Nem szabad globálisba kitenni a userDatas-t!
   var userDatas = JSON.parse(xhttp.responseText);
+  var livingUsers = livingCharacters(userDatas);
   // Innen hívhatod meg a többi függvényed
-  sortCharactersByName(userDatas);
-  selectCharactersPortraitAndName(userDatas);
+  sortCharactersByName(livingUsers);
+  selectCharactersPortraitAndName(livingUsers);
   searchCharacterTextButton();
-  clickButton(userDatas);
+  clickButton(livingUsers);
 }
 getGameOfThronesCharacterDatas(
   './json/got.json',
@@ -26,6 +27,16 @@ getGameOfThronesCharacterDatas(
 
 // Live servert használd mindig!!!!!
 /* IDE ÍRD A FÜGGVÉNYEKET!!!!!! NE EBBE AZ EGY SORBA HANEM INNEN LEFELÉ! */
+function livingCharacters(user) {
+  var livingCharactersData = [];
+  for (var i = 0; i < user.length; i++) {
+    if (!user[i].dead) {
+      livingCharactersData.push(user[i]);
+    }
+  }
+  return livingCharactersData;
+}
+
 function sortCharactersByName(user) {
   for (var i = 0; i < user.length - 1; i++) {
     for (var j = i + 1; j < user.length; j++) {
@@ -43,7 +54,7 @@ function selectCharactersPortraitAndName(user) {
   for (var i = 0; i < user.length; i++) {
     characterRow += `
                     <div class=div__character__portrait>
-                    <img src=${user[i].portrait} alt="portrait">
+                    <img src=${user[i].portrait} alt="portrait" onclick="clickOnPortrait()">
                     <p>${user[i].name}</p>
                     </div>
                     `;
@@ -56,23 +67,42 @@ function searchCharacterTextButton() {
                           <input class="search__input" type="text" placeholder="Search a character">
                           <input class="search__button" type="button" value="Search">`;
 }
+function characterOnDisplay(found) {
+  var descriptionCharacter = document.querySelector('.div__description__character');
+  var house = '';
+  if (found[0].house) {
+    house = `<img src="/assets/houses/${found[0].house}.png">`;
+  } else {
+    house = '';
+  }
+  descriptionCharacter.innerHTML = `
+                                        <p><img src=${found[0].picture} alt="picture"></p>
+                                        <p>${found[0].name}</p>
+                                        <p>${house}</p>
+                                        <p>${found[0].bio}</p>
+                                       `;
+  document.querySelector('.search__input').value = '';
+}
+function characterNoFound() {
+  var noDescription = document.querySelector('.div__description__character');
+  noDescription = 'Character not found.';
+  document.querySelector('.search__input').value = '';
+}
 
+function searchCharacter(user) {
+  var userInput = document.querySelector('.search__input').value;
+  userInput = userInput.toLowerCase();
+  var found = [];
+  for (var i = 0; i < user.length; i++) {
+    if ( userInput === user[i].name.toLowerCase()) {
+      found.push(user[i]);
+      characterOnDisplay(found);
+    } else {
+      characterNoFound();
+    }
+  }
+}
 function clickButton(user) {
   var searchButton = document.querySelector('.search__button');
-  searchButton.addEventListener('click', function searchCharacter() {
-    var userInput = document.querySelector('.search__input').value;
-    userInput = userInput.toLowerCase();
-    var descriptionCharacter = document.querySelector('.div__description__character');
-    for (var i = 0; i < user.length; i++) {
-      if ( userInput === user[i].name.toLowerCase()) {
-        descriptionCharacter.innerHTML = `
-                                        <p><img src=${user[i].picture} alt="picture"></p>
-                                        <p>${user[i].name}</p>
-                                        <p>${user[i].bio}</p>
-                                       `;
-      } else {
-        descriptionCharacter.innerHTML = 'Character not found.';
-      }
-    }
-  });
+  searchButton.addEventListener('click', function () {searchCharacter(user);});
 }
